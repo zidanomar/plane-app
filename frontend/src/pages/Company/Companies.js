36f76d4fcc102bd4b0c61c3785e68ee4abd39ce2
Zidan Omar Hamid <hzidanomar@gmail.com>
2022-01-19 14:56:19 +0300

    companies page card

--- frontend/src/pages/Company/Companies.js
+++ frontend/src/pages/Company/Companies.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import { Container, Grid, Link } from '@chakra-ui/react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link as ReachLink } from 'react-router-dom';
+
+import CompanyCard from '../../components/Cards/CompanyCard';
+import { getAllCompany } from '../../flux/actions/companyAction';
 
 function Companies() {
-  return <div>companies</div>;
+  const dispatch = useDispatch();
+  const companies = useSelector((state) => state.company.companies);
+
+  useEffect(() => {
+    dispatch(getAllCompany());
+  }, [dispatch]);
+
+  return (
+    <Container maxW='container.xl'>
+      <Grid
+        templateColumns={{
+          sm: 'repeat(1, 1fr)',
+          md: 'repeat(2, 1fr)',
+          lg: 'repeat(3, 1fr)',
+          xl: 'repeat(4, 1fr)',
+        }}
+        gap={6}
+      >
+        {companies.map((c) => (
+          <Link key={c.uuid} as={ReachLink} to={`/companies/${c.uuid}`}>
+            <CompanyCard name={c.name} totalPlane={c.planes.length} />
+          </Link>
+        ))}
+      </Grid>
+    </Container>
+  );
 }
 
 export default Companies;
